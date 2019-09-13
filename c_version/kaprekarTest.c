b27bd8f2969c0be904377ce01bd545acd0aac967
zbuhman <54590432+zbuhman@users.noreply.github.com>
2019-09-13 02:18:58 -0500

    Revert "Update kaprekarTest.c"
    This reverts commit 0cb930d66eb4aa8b3c44bcd1e0308711f5b2d93f.

--- c_version/kaprekarTest.c
+++ c_version/kaprekarTest.c
@@ -30,13 +30,11 @@ int main(int argc, char **argv) {
     if(contains(kaprekarNumbers, numKaprekarNumbers, i) != isKaprekar(i)) {
       numWrong++;
       if(contains(kaprekarNumbers, numKaprekarNumbers, i)) {
-	       printf(stderr, "i = %d: is a known Kaprekar number, but the method returned false\n", i);
+	printf(stderr, "i = %d: is a known Kaprekar number, but the method returned false\n", i);
+      } else {
+	printf(stderr, "i = %d: is known NOT to be a Kaprekar number, but the method returned true\n", i);
       }
-      else {
-	       printf(stderr, "i = %d: is known NOT to be a Kaprekar number, but the method returned true\n", i);
-      }
-    }
-    else {
+    } else {
       numCorrect++;
     }
   }
